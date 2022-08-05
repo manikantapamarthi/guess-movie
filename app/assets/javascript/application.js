ca9a3de1e5dd08cb62d637b955c8ddd7ad84277a
Manikanta <manikantassp@gmail.com>
2022-08-05 07:47:39 +0530

    javascript location changed

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,6 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
-import "./controllers/main_controller";
-import "./controllers/movies_list_controller";
-import "./controllers/share_controller";
+import "controllers";
 import "bootstrap";
