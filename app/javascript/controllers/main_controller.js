a3e4dcb99fb7a830165fef4d3c06f534bb559130
Manikanta Nagaraju Pamarthi <manikantassp@gmail.com>
2022-11-02 23:30:27 +0530

    new movie name added2

--- app/javascript/controllers/main_controller.js
+++ app/javascript/controllers/main_controller.js
@@ -173,6 +173,9 @@ export default class extends Controller {
       resultItem: {
         highlight: true
       },
+      resultsList: {
+        maxResults: -1
+      },
       events: {
         input: {
           selection: (event) => {
